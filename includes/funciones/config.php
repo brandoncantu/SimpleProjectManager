9de445afc1ed442c16fba16e1ac6bc9fb38accaa
Brandon Cantu <brandon.2g.05@gmail.com>
2020-12-01 00:50:16 -0600

    Heroku link

--- includes/funciones/config.php
+++ includes/funciones/config.php
@@ -1,7 +1,7 @@
 <?php
     $user = 'bf9c21fc2d651a';
     $password = 'dd70c8e7';
-    $db = '`heroku_f2eef08d6f776c1`';
+    $db = 'heroku_f2eef08d6f776c1';
     $host = 'us-cdbr-east-02.cleardb.com';
 
     $link = mysqli_connect($host, $user, $password, $db);
