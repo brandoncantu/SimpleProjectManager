883637789b57dff2b2752dcd6c3f3b6a253f9856
Brandon Cantu <brandon.2g.05@gmail.com>
2020-12-01 01:28:15 -0600

    Config echo

--- includes/funciones/config.php
+++ includes/funciones/config.php
@@ -8,7 +8,7 @@
         if(!$link){
             die("Connection failed: " . mysqli_connect_error());
         }else{
-            //echo "Connection succesfull!";
+            echo "Connection succesfull!";
         }
 
 ?>
