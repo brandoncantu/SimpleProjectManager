ff02275d17490672ab1e84e7366b0f3365373e15
Brandon Cantu Lozano <74885848+brandoncantu@users.noreply.github.com>
2020-12-01 00:25:19 -0600

    Update config.php

--- includes/funciones/config.php
+++ includes/funciones/config.php
@@ -1,7 +1,7 @@
 <?php
     define('DB_USUARIO', 'bf9c21fc2d651a');
     define('DB_PASSWORD', 'dd70c8e7');
-    define('DB_HOST', 'localhost:3307');
+    define('DB_HOST', 'us-cdbr-east-02.cleardb.com');
     define('DB_NOMBRE', '`heroku_f2eef08d6f776c1`');
 
     $link = new mysqli(DB_HOST, DB_USUARIO, DB_PASSWORD, DB_NOMBRE );
